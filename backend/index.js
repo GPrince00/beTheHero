f659d24dd859eeae223d210e6c9d8824e9fcd0f7
GPrince00 <gustavoprince0@gmail.com>
2020-03-25 20:38:27 -0300

    feat(nodemon): configures nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.post('/users', (req, res) => {
     return res.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Prince'
     });
 });
 
-app.listen(3333);  
+app.listen(3333);
