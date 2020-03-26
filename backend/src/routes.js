7fd674fbb7fe98212bb0f31c3d65cc0289d1e894
GPrince00 <gustavoprince0@gmail.com>
2020-03-26 12:55:29 -0300

    feat(ongs): creates an insert to incidents table

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngControllers');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
