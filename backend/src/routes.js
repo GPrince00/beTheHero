578fb482e822b347badb47bbe09806ce7f5fd1ec
GPrince00 <gustavoprince0@gmail.com>
2020-03-25 22:47:05 -0300

    feat(ongs): creates an insert to ongs table

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,9 +4,21 @@ const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/ongs', (req, res) => {
-    
-    return res.json();
+routes.post('/ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return res.json({ id });
 });
 
 
