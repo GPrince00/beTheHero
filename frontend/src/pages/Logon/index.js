cf30dbfdd26f8f7b84f53f7edd4e43aa0768f2b9
GPrince00 <gustavoprince0@gmail.com>
2020-03-27 12:02:28 -0300

    bugfix

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -4,6 +4,27 @@ import './styles.css';
 
 export default function Logon() {
     return (
+<<<<<<< HEAD
         <h1>Hello World</h1>
+=======
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+                <form>
+                    <h1>Faça seu login</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+>>>>>>> parent of a0e3a84... feat(login): adds a styles on login page
     )
 }
