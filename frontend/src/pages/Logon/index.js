fe96b8146a9eb1a1a7a2336e6c60d9424de170f9
GPrince00 <gustavoprince0@gmail.com>
2020-03-27 12:01:13 -0300

    bugfix

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -4,23 +4,6 @@ import './styles.css';
 
 export default function Logon() {
     return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={logoImg} alt="Be The Hero" />
-                <form>
-                    <h1>Faça seu login</h1>
-
-                    <input placeholder="Sua ID" />
-                    <button className="button" type="submit">Entrar</button>
-
-                    <a href="/register">
-                        <FiLogIn size={16} color="#e02041" />
-                        Não tenho cadastro
-                    </a>
-                </form>
-            </section>
-
-            <img src={heroesImg} alt="Heroes" />
-        </div>
+        <h1>Hello World</h1>
     )
 }
