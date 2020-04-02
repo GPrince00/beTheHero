2b31aa4b94bac1ea3164eb6bbdd4f2b582442d0d
GPrince00 <gustavoprince0@gmail.com>
2020-04-01 22:24:43 -0300

    feat(register): adds useHistory

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../servises/api';
@@ -13,6 +13,8 @@ export default function Register() {
     const [city, setCity] = useState('');
     const [uf, setUf] = useState('');
 
+    const history = useHistory();
+
     async function handleRegister(e) {
         e.preventDefault();
 
@@ -25,11 +27,12 @@ export default function Register() {
         };
         
         try {
-            const response = await api.post('ongs', data)
+            const response = await api.post('ongs', data);
     
-            alert(`Seu ID de acesso: ${response.data.id}`)
+            alert(`Seu ID de acesso: ${response.data.id}`);
+            history.push('/');
         } catch (err) {
-            alert('Erro no cadastro, tente novamente.')
+            alert('Erro no cadastro, tente novamente.');
         }
 
     }
